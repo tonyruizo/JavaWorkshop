6e015154b3313bb9e634fb6eed5ec5bea07e4e96
Tonyruizo <tonyruizo@pm.me>
2023-08-25 16:22:20 -0400

    Initial commit

---
+++ exercise1_1/src/exercise1_1/Welcome.java
@@ -0,0 +1,16 @@
+// Tony Ruiz Ormeno
+// 8/25/23
+// Exercise 1.1
+
+package exercise1_1;
+// Define a public Welcome class 
+public class Welcome {
+	// Define a void main method
+	public static void main(String[] args) {
+		// Print out into console
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun");
+	}
+
+}
