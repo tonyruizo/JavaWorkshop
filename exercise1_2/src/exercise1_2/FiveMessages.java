6b6345260661df001139be17ff7ad8f1629a227c
Tonyruizo <tonyruizo@pm.me>
2023-09-03 10:36:03 -0400

    Modified exercise

--- exercise1_2/src/exercise1_2/FiveMessages.java
+++ exercise1_2/src/exercise1_2/FiveMessages.java
@@ -1,7 +1,25 @@
+// Tony Ruiz Ormeno
+// 9/3/23
+// Exercise 1.2
+
 package exercise1_2;
 
+// Define a public FiveMessages Class
 public class FiveMessages {
+	// Define a void main method
 	public static void main(String[] args) {
-		System.out.print("Testing");
+		
+		// Calling the displayMsg method five times
+		displayMsg();
+		displayMsg();
+		displayMsg();
+		displayMsg();
+		displayMsg();
+	}
+	
+	// Method to display/print out the message
+	static void displayMsg() {
+		
+		System.out.print("Welcome to Java.\n");
 	}
 }
