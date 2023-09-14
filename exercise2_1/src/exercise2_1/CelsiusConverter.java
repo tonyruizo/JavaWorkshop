667ac286f6b93e3b3629716c3cc8dbfdfeda5a80
Tonyruizo <tonyruizo@pm.me>
2023-09-14 08:28:36 -0400

    Initial Exercise

---
+++ exercise2_1/src/exercise2_1/CelsiusConverter.java
@@ -0,0 +1,27 @@
+// Tony Ruiz Ormeno
+
+package exercise2_1;
+
+import java.util.*;
+
+public class CelsiusConverter {
+
+	public static void main(String[] args) {
+		
+		// Ask user to enter celsius
+		System.out.println("Enter a degree in Celsius: ");
+		
+		// Get user's input
+		Scanner userInput = new Scanner(System.in);
+		
+		// Read user's input
+		double celsius = userInput.nextDouble();
+		
+		// Calculation Conversion
+		double fahrenheit = 1.8 * celsius + 32;
+		
+		// Display Result
+		System.out.println(celsius + " Celsius is " + fahrenheit + " Fahrenheit.");
+	}
+
+}
