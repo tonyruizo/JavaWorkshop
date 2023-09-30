799da7a2b43832328a4873c8496e8b23051af028
Tonyruizo <tonyruizo@pm.me>
2023-09-30 19:51:35 -0400

    Update

--- exercise4_4/AreaOfHexagon.java
+++ exercise4_4/AreaOfHexagon.java
@@ -12,7 +12,16 @@ public class AreaOfHexagon {
 
     // Prompt user to enter input
     System.out.println("Enter the side: \n");
+    double side = userInput.nextDouble();
 
+    // Calculate using formula
+    double area = (6 * Math.pow(side)) / (4 * Math.tan(PI / 6));
+
+    // Display to user
+    System.out.println("The area of the hexagon is " + area);
+
+    // Close scanner
     userInput.close();
   }
+
 }
